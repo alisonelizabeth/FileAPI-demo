0b0c0258b902020428d9f42acd941fbcafac4449
alisonelizabeth <alisonmllr20@gmail.com>
2013-12-12 12:01:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){      
+    $("#file").change(function(){
+        readURL(this);
+    });
+});
+// end of document ready
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+    	console.log(input.files)
+    	// files is a FileList of File objects.
+    	
+        var reader = new FileReader();
+        console.log(reader)
+        // FileReader provides methods to read File
+
+        reader.onload = function(e) {
+            $('#image-preview').attr('src', e.target.result);
+            console.log(e.target.result)
+        }
+        // When the load finishes, the reader's onload event is fired
+        // Result attribute is used to access file data
+
+        reader.readAsDataURL(input.files[0]);
+        // FileReader has four options for reading a file
+        // One of which is FileReader.readAsDataURL(File)
+        // Contains data URL for file
+        // *this is set up to only preview one image
+    }
+}   
+
+// Resources:
+// http://www.w3.org/TR/file-upload/
+// http://www.html5rocks.com/en/tutorials/file/dndfiles/ 
